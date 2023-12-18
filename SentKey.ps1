5d4529c8b3bfcb8426a5cd6512d3fdb62ddd2d33
sean0o0 <63410304+foxcatch@users.noreply.github.com>
2023-12-18 09:54:17 +0100

    Add files via upload
    Sent a key automatically every 2 seconds.

---
+++ SentKey.ps1
@@ -0,0 +1,12 @@
+﻿Write-Host 'Job start'
+
+$shell = New-Object -ComObject "Wscript.Shell"
+    
+while($True)
+{
+# Start-Sleep -Seconds 100
+Start-Sleep -Seconds 200
+Write-Host "Running ..."
+# $shell.SendKeys(" 0")
+$shell.SendKeys(" ")
+}
